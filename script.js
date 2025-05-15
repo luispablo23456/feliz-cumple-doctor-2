eee6c38a62cb406f676b0fffd2fe12cab8ea3e33
luispablo23456 <luispablogarcesmorales2@gmail.com>
2025-05-14 21:37:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY', 'BIRTHDAY!','My Love' ],
+			strings: [ 'FELIZ', 'CUMPLEAÑOS!','DOCTORCITO','TE AMO' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
